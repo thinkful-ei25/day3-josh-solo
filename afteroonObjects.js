f5d8164f2a1758ac2b3dd4eb6999fef3bdb85319
Joshua Adams <joshadams554@gmail.com>
2018-09-26 20:25:26 -0400

    hobbitses

--- afteroonObjects.js
+++ afteroonObjects.js
@@ -7,3 +7,21 @@ const loaf = ({
 })
 
 console.log(`${loaf.flour} \n${loaf.water} \n${loaf.hydration()}`);
+
+const itObj = ({
+    foo: 'foo',
+    bar: 'bar',
+    fum: 100,
+    quuz: [1,2,3],
+    spam: 'Nigerian Prince'
+})
+
+for(thing in itObj){
+    console.log(thing + ' = ' +itObj[thing]);
+}
+
+const hobbitFeed = ({
+    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+})
+
+console.log(hobbitFeed.meals[3]);
